0fec88a725f41e918660ea676bbbe7705d6fd0e9
mskuroedov <mskuroedov@gmail.com>
2018-08-10 15:01:45 +0300

    Update demo2.js

--- js/demo2.js
+++ js/demo2.js
@@ -4,7 +4,7 @@
 	var canvas = document.querySelector('#webgl');
 
 	// Scroll variables
-	var scroll = 0.0, velocity = 0.0, lastScroll = 0.0;
+	var scroll = 0.0, velocity = 5.0, lastScroll = 0.0;
 
 	// Initialize REGL from a canvas element
 	var regl = createREGL({
@@ -31,7 +31,7 @@
 				resolution: regl.prop('resolution'),
 				aspect: regl.prop('aspect'),
 				scroll: regl.prop('scroll'),
-				velocity: 5000,
+				velocity: regl.prop('velocity'),
 				texture: regl.texture(img)
 			}
 		});
